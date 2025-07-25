861f4b2fcebc37c41087ba1e9e30c38d1b3ede54
Kevin UDIT <kevincutepa@gmail.com>
2025-07-24 19:43:20 -0600

    feat() => Demonization lib

--- daemon.c
+++ daemon.c
@@ -1,2 +1,197 @@
-#include <daemon.h>
+/*
+ * Based on daemonization template by KoynovStas.
+ * Original source: https://github.com/KoynovStas/daemon_templates.git
+ * Licensed under the BDS-3 License.
+ */
 
+#include "daemon.h"
+
+#include <errno.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/file.h>
+#include <unistd.h>
+
+volatile struct daemon_info_t daemon_info = {
+    // flag will be set in finale function daemonize()
+    .terminated = 0,
+    .daemonized = 0,
+
+#ifdef DAEMON_NO_CHDIR
+    .no_chdir = DAEMON_NO_CHDIR,
+#else
+    .no_chdir = 0,
+#endif
+
+#ifdef DAEMON_NO_FORK
+    .no_fork = DAEMON_NO_FORK,
+#else
+    .no_fork = 0,
+#endif
+
+#ifdef DAEMON_NO_CLOSE_STDIO
+    .no_close_stdio = DAEMON_NO_CLOSE_STDIO,
+#else
+    .no_close_stdio = 0,
+#endif
+
+#ifdef DAEMON_PID_FILE_NAME
+    .pid_file = DAEMON_PID_FILE_NAME,
+#else
+    .pid_file = NULL,
+#endif
+
+#ifdef DAEMON_LOG_FILE_NAME
+    .log_file = DAEMON_LOG_FILE_NAME,
+#else
+    .log_file = NULL,
+#endif
+
+#ifdef DAEMON_CMD_PIPE_NAME
+    .cmd_pipe = DAEMON_CMD_PIPE_NAME,
+#else
+    .cmd_pipe = NULL,
+#endif
+};
+
+// Exit if the daemon is not daemonized
+// This is used to ensure that the daemon is running in the background
+void exit_if_not_daemonized(int exit_status) {
+    if (!daemon_info.daemonized) _exit(exit_status);
+}
+
+/*
+ * This function is used to exit the daemon with an error message.
+ * It uses variable arguments to format the error message.
+ * It will print the error message to stderr and then exit the daemon.
+ * The exit status will be EXIT_FAILURE.
+ */
+void daemon_error_exit(const char *format, ...) {
+    va_list ap;
+
+    if (format && *format) {
+        va_start(ap, format);
+        fprintf(stderr, "%s: ", DAEMON_NAME);
+        vfprintf(stderr, format, ap);
+        va_end(ap);
+    }
+
+    _exit(EXIT_FAILURE);
+}
+
+int redirect_stdio_to_devnull(void) {
+    int fd;
+
+    fd = open("/dev/null", O_RDWR);
+    if (fd == -1) return -1;
+
+    dup2(fd, STDIN_FILENO);
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+
+    if (fd > 2) close(fd);
+
+    return 0;
+}
+
+int create_pid_file(const char *pid_file_name) {
+    int fd;
+    const int BUF_SIZE = 32;
+    char pid_str[BUF_SIZE];
+
+    if (!pid_file_name) {
+        fprintf(stderr, "PID file name is NULL\n");
+        return -1;
+    }
+
+    fd = open(pid_file_name, O_RDWR | O_CREAT, 0644);
+    if (fd < 0) {
+        errno = EINVAL;
+        fprintf(stderr, "Failed to open PID file '%s': %s\n", pid_file_name, strerror(errno));
+        return -1;
+    }
+
+    // Try to lock the file to ensure only one instance of the daemon can run
+    if (lockf(fd, F_TLOCK, 0) == -1) {
+        close(fd);
+        return -1;
+    }
+
+    // Clear the file content before writing the new PID
+    if (ftruncate(fd, 0) != 0) {
+        close(fd);
+        return -1;  // Could not truncate on PID file
+    }
+
+    snprintf(pid_str, BUF_SIZE, "%ld\n", (long)getpid());
+    if (write(fd, pid_str, strlen(pid_str)) != (int)strlen(pid_str)) {
+        close(fd);
+        return -1;  // Could not write PID to PID file
+    }
+
+    return fd;
+}
+
+/*
+ * Set a signal handler for a specific signal.
+ * This function uses the signal() system call to set the handler.
+ * If the handler cannot be set, it will exit with an error message.
+ */
+void set_sig_handler(int sig, signal_handler_t handler) {
+    if (signal(sig, handler) == SIG_ERR)
+        daemon_error_exit("Can't set handler for signal: %d %m\n", sig);
+    // ojo piojo %m its for gnu libc to print the error message
+}
+
+/*
+Actual demonization function.
+This function will fork the process, but its not changing the directory to "/",
+not closing the standard IO files and not changing to a new session (SID).
+Just forking the process.
+*/
+static void do_fork() {
+    switch (fork())  // Become background process
+    {
+        case -1:
+            daemon_error_exit("Can't fork: %m\n");
+        case 0:
+            break;  // child process (go next)
+        default:
+            _exit(EXIT_SUCCESS);  // We can exit the parent process
+    }
+
+    // ---- At this point we are executing as the child process ----
+}
+
+/*
+This functions handles the demonization options.
+*/
+void daemonize2(void (*optional_init)(void *), void *data) {
+    if (!daemon_info.no_fork) do_fork();
+
+    // Reset the file mode mask
+    umask(0);
+
+    // Create a new process group(session) (SID) for the child process
+    // call setsid() only if fork is done
+    if (!daemon_info.no_fork && (setsid() == -1)) daemon_error_exit("Can't setsid: %m\n");
+
+    // Change the current working directory to "/"
+    // This prevents the current directory from locked
+    // The demon must always change the directory to "/"
+    if (!daemon_info.no_chdir && (chdir("/") != 0)) daemon_error_exit("Can't chdir: %m\n");
+
+    if (daemon_info.pid_file && (create_pid_file(daemon_info.pid_file) == -1))
+        daemon_error_exit("Can't create pid file: %s: %m\n", daemon_info.pid_file);
+
+    // call user functions for the optional initialization
+    // before closing the standardIO (STDIN, STDOUT, STDERR)
+    if (optional_init) optional_init(data);
+
+    if (!daemon_info.no_close_stdio && (redirect_stdio_to_devnull() != 0))
+        daemon_error_exit("Can't redirect stdio to /dev/null: %m\n");
+
+    daemon_info.daemonized = 1;  // good job
+}
--- daemon.h
+++ daemon.h
@@ -1,6 +1,14 @@
 #ifndef DAEMON_H
 #define DEAMON_H
 
+/*
+ * Based on daemonization template by KoynovStas.
+ * Original source: https://github.com/KoynovStas/daemon_templates.git
+ * Licensed under the BDS-3 License.
+ */
+
+#include <stddef.h>  //for null
+
 #define DAEMON_NAME "balrog"
 
 #define DAEMON_DEF_TO_STR_(text) #text
@@ -10,24 +18,24 @@
 #define DAEMON_MINOR_VERSION_STR DAEMON_DEF_TO_STR(DAEMON_MINOR_VERSION)
 #define DAEMON_PATCH_VERSION_STR DAEMON_DEF_TO_STR(DAEMON_PATCH_VERSION)
 
-#define DAEMON_VERSION_STR DAEMON_MAJOR_VERSION_STR "." DAEMON_MINOR_VERSION_STR "." DAEMON_PATCH_VERSION_STR
+#define DAEMON_VERSION_STR \
+    DAEMON_MAJOR_VERSION_STR "." DAEMON_MINOR_VERSION_STR "." DAEMON_PATCH_VERSION_STR
 
 /*
-    * This is the daemon info structure, it contains all the information
-    * that the daemon needs to run, like the pid file, log file, cmd pipe,
-    * and some flags to control the daemon behavior.
-    * The flags are:
-    * - terminated: if the daemon is terminated
-    * - daemonized: if the daemon is daemonized
-    * - no_chdir: if the daemon should not change the directory to '/'
-    * - no_fork: if the daemon should not fork
-    * - no_close_stdio: if the daemon should not close the standard IO files
-    * - pid_file: the pid file name
-    * - log_file: the log file name
-    * - cmd_pipe: the command pipe name
-*/
-struct daemon_info_t
-{
+ * This is the daemon info structure, it contains all the information
+ * that the daemon needs to run, like the pid file, log file, cmd pipe,
+ * and some flags to control the daemon behavior.
+ * The flags are:
+ * - terminated: if the daemon is terminated
+ * - daemonized: if the daemon is daemonized
+ * - no_chdir: if the daemon should not change the directory to '/'
+ * - no_fork: if the daemon should not fork
+ * - no_close_stdio: if the daemon should not close the standard IO files
+ * - pid_file: the pid file name
+ * - log_file: the log file name
+ * - cmd_pipe: the command pipe name
+ */
+struct daemon_info_t {
     // flags
     unsigned int terminated : 1;
     unsigned int daemonized : 1;
@@ -41,5 +49,16 @@ struct daemon_info_t
 };
 
 extern volatile struct daemon_info_t daemon_info;
+
+void exit_if_not_daemonized(int exit_status);
+void daemon_error_exit(const char *format,
+                       ...);  // this one use variable args with the lib stdarg.h
+int create_pid_file(const char *pid_file_name);
+
+typedef void (*signal_handler_t)(int);
+void set_sig_handler(int sig, signal_handler_t handler);
+
+void daemonize2(void (*optional_init)(void *), void *data);
+static inline void daemonize() { daemonize2(NULL, NULL); }
 
 #endif
